94225b702eec86b52cbbe99729cffba344fab5b0
Marika Basagoitia <marika.basagoitia@gmail.com>
2026-06-15 19:43:05 -0400

    feat(ui): add event listener to play button

--- src/components/audio/PlaybackControls/PlaybackControls.tsx
+++ src/components/audio/PlaybackControls/PlaybackControls.tsx
@@ -1,7 +1,16 @@
+import { useCurrentScene } from "../../../services/useCurrentScene";
+
 export function PlaybackControls() {
+
+  const scene = useCurrentScene();
+
+  const handlePlay = () => {
+    console.log("Playing scene:", scene);
+  };
+
   return (
     <>
-      <button>Play</button>
+      <button onClick={handlePlay}>Play</button>
       <button>Pause</button>
       <button>Stop</button>
     </>
