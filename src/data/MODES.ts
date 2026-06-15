5cac734f50c7dacdc90d9c6ec451e1ed60f43b26
Marika Basagoitia <marika.basagoitia@gmail.com>
2026-06-15 19:32:47 -0400

    feat(data): add modes with scale degrees

---
+++ src/data/MODES.ts
@@ -0,0 +1,10 @@
+export const MODES = {
+  ionian:     [0, 2, 4, 5, 7, 9, 11],
+  dorian:     [0, 2, 3, 5, 7, 9, 10],
+  phrygian:   [0, 1, 3, 5, 7, 8, 10],
+  lydian:     [0, 2, 4, 6, 7, 9, 11],
+  mixolydian: [0, 2, 4, 5, 7, 9, 10],
+  aeolian:    [0, 2, 3, 5, 7, 8, 10],
+  locrian:    [0, 1, 3, 5, 6, 8, 10],
+  wholeTone:  [0, 2, 4, 6, 8, 10]
+};
