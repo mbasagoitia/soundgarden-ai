07c887243a88de78a13ee4b4a65edfb19c4b8285
Marika Basagoitia <marika.basagoitia@gmail.com>
2026-06-15 19:41:27 -0400

    refactor(service): add tempo parameter to sceneBuilder

--- src/services/sceneBuilder.ts
+++ src/services/sceneBuilder.ts
@@ -1,17 +1,18 @@
 import { type PresetType } from "../components/audio/PresetsSelect/PresetsSelect";
-import { Scene } from "../domain/Scene";
+import { type Scene } from "../domain/Scene";
 import { harmonyPresets } from "../data/harmonyPresets";
 import { moodPresets } from "../data/moodPresets";
 import { texturePresets } from "../data/texturePresets";
+import { deriveTempo } from "./deriveTempo";
+
 
 export function buildScene(
   preset: PresetType
 ): Scene {
   return {
     mood: moodPresets[preset],
-
+    tempo: deriveTempo(moodPresets[preset]),
     harmony: harmonyPresets[preset],
-
     texture: texturePresets[preset],
   };
 }
