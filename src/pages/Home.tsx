7bb854c603f473cd7c9f6f2c1a344b236536217e
Marika Basagoitia <marika.basagoitia@gmail.com>
2026-06-15 19:48:01 -0400

    feat(ui): render presents and save user choice to app store

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,26 @@
-import { useState } from 'react';
-import { PresetsSelect, type PresetType } from '../components/audio/PresetsSelect/PresetsSelect';
+import { useAppStore } from '../store/useAppStore';
+import { PresetsSelect } from '../components/audio/PresetsSelect/PresetsSelect';
 import { PlaybackControls } from '../components/audio/PlaybackControls/PlaybackControls';
 
+
 export function Home() {
-  const [preset, setPreset] = useState<PresetType>('sunrise');
+
+  const selectedPreset =
+  useAppStore(
+    (state) => state.selectedPreset
+  );
+
+  const setSelectedPreset =
+  useAppStore(
+    (state) => state.setSelectedPreset
+  );
 
   return (
     <>
       <h1>Sound Garden</h1>
       <PresetsSelect
-        selectedPreset={preset}
-        onSelect={setPreset}
+        selectedPreset={selectedPreset}
+        onSelect={setSelectedPreset}
       />
       <PlaybackControls />
     </>
