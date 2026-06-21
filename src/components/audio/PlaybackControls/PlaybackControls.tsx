ce45521801434f951f76adace72f4b3d6f281029
Marika Basagoitia <marika.basagoitia@gmail.com>
2026-06-21 18:51:51 -0400

    feat(ui): change Playback Controls to pass scene to createComposition

--- src/components/audio/PlaybackControls/PlaybackControls.tsx
+++ src/components/audio/PlaybackControls/PlaybackControls.tsx
@@ -1,11 +1,17 @@
+import { createComposition } from "../../../services/createComposition";
 import { useCurrentScene } from "../../../services/useCurrentScene";
 
 export function PlaybackControls() {
 
   const scene = useCurrentScene();
 
+
   const handlePlay = () => {
-    console.log("Playing scene:", scene);
+    console.log("Playing composition...");
+    if (scene) {
+    console.log(createComposition(scene));
+    }
+
   };
 
   return (
